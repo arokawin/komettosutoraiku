4437b0563fc6fb3fc98a600e63b9d47bc7a1b6ff
arokawin <kiy289522@gmail.com>
2025-05-29 10:00:08 +0900

    コードの整理

--- Assets/kiyosawa/Scripts/RandomUnder.cs
+++ Assets/kiyosawa/Scripts/RandomUnder.cs
@@ -6,7 +6,7 @@ public class RandomUnder : MonoBehaviour
 {
     [SerializeField] private GameObject inseki;
     [SerializeField] private GameObject _inseki;
-    // Start is called before the first frame update
+
     void Start()
     {
         // ŒJ‚è•Ô‚µˆ—
@@ -14,11 +14,6 @@ public class RandomUnder : MonoBehaviour
         InvokeRepeating("Randomkougeki_Right", 3f, 3f);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
     void Randomkougeki_Left()
     {
         Instantiate(inseki,new Vector2(Random.Range(9.4f, -13f),transform.position.y),

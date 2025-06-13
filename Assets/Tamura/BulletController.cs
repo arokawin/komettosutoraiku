9bef129f17240a424dc16d4785507178fcb5d0a8
Met115 <metarucop@gmail.com>
2025-06-13 13:35:22 +0900

    弾の向きを修正

--- Assets/Tamura/BulletController.cs
+++ Assets/Tamura/BulletController.cs
@@ -6,21 +6,28 @@ public class BulletController : MonoBehaviour
 {
     [SerializeField,Header("�e�̑��x")]
     private float speed;
-    private Vector2 direction = Vector2.right;
 
-    // �e�̌�����O������ݒ�
+    private Vector2 direction;
+
+    private static readonly Vector2 SpriteDir = new Vector2(-1f, -1f).normalized;
+
     public void SetDirection(Vector2 dir)
     {
         direction = dir.normalized;
+
+        float angle = Vector2.SignedAngle(SpriteDir, direction);
+
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
     }
 
     void Update()
     {
-        transform.Translate(direction * speed * Time.deltaTime);
+
+        transform.position += (Vector3)direction * speed * Time.deltaTime;
     }
 
     private void OnBecameInvisible()
     {
-        Destroy(gameObject); // ��ʊO�ɏo����j��
+        Destroy(gameObject);
     }
 }

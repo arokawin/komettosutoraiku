640462a2b202373f40919fb345201c590ce38aef
Met115 <metarucop@gmail.com>
2025-07-11 17:54:43 +0900

    ブロック差し替え、移動プログラム修正

--- Assets/Tamura/PlayerController.cs
+++ Assets/Tamura/PlayerController.cs
@@ -91,7 +91,7 @@ public class PlayerController : MonoBehaviour
 
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, GroundLayer);
 
-      transform.position += new Vector3(move.x, 0f, 0f) * xSpeed * Time.deltaTime;
+     // transform.position += new Vector3(move.x, 0f, 0f) * xSpeed * Time.deltaTime;
 
         anim.SetBool("Move", move.x != 0);
         if (move.x < 0)
@@ -259,10 +259,10 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // 移動速度ベクトルを現在値から取得
+        if (gameManager.GetComponent<GameManager>().gameEnd == true) return;
         Vector2 velocity = rb2d.velocity;
-        // X方向の速度を入力から決定
-        velocity.x = xSpeed;
+        velocity.x = move.x * xSpeed;
+        rb2d.velocity = velocity;
     }
 
 

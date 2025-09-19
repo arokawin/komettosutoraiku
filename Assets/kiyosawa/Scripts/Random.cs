d06b36f07b92a55bd65e47d253b2998664ad7d34
arokawin <kiy289522@gmail.com>
2025-09-19 14:47:54 +0900

    コードフリーズ

--- Assets/kiyosawa/Scripts/Random.cs
+++ Assets/kiyosawa/Scripts/Random.cs
@@ -20,10 +20,12 @@ public class RandomMeteo : MonoBehaviour
         switch (UpDown)
         {
             case 0:
+                // 隕石の生成、生成する場所のランダム、オブジェクトの回転
                 var meteo = Instantiate(inseki, new Vector2(Random.Range(rangeA.position.x, rangeB.position.x), -10f), Quaternion.Euler(0, 0, 90f));
                 var rb = meteo.GetComponent <Rigidbody2D>();
                 var Xvec = Random.Range(-10f, 10f);
                 var Yvec = Random.Range(1f, 10f);
+                // 進んでいる方向を計算
                 var radY = Mathf.Acos(Xvec / Mathf.Sqrt(Mathf.Pow(Xvec, 2) + Mathf.Pow(Yvec, 2))) * Mathf.Rad2Deg;
                 // if (meteo.transform.position.x >= 0.01f) meteo.transform.localEulerAngles = new Vector3(0, 0, -90f);
                 // else meteo.transform.localEulerAngles = new Vector3(0, 0, 180f);
@@ -32,15 +34,15 @@ public class RandomMeteo : MonoBehaviour
 
                 break; 
             case 1:
-                meteo = Instantiate(inseki, new Vector2(Random.Range(rangeA.position.x, rangeB.position.x), 10f), Quaternion.Euler(0, 0, 90f));
-                rb = meteo.GetComponent<Rigidbody2D>();
-                Xvec = Random.Range(-10f, 10f);
-                Yvec = Random.Range(-1f, -10f);
-                radY = Mathf.Acos(Xvec / Mathf.Sqrt(Mathf.Pow(Xvec, 2) + Mathf.Pow(Yvec, 2))) * Mathf.Rad2Deg;
-                // if (meteo.transform.position.x >= 0.01f) meteo.transform.localEulerAngles = new Vector3(0, 0, -90f);
-                // else meteo.transform.localEulerAngles = new Vector3(0, 0, 180f);
-                meteo.transform.localEulerAngles = new Vector3(0, 0, meteo.transform.localEulerAngles.z - radY);
-                rb.velocity = new Vector3(Xvec, Yvec, 0);
+                //meteo = Instantiate(inseki, new Vector2(Random.Range(rangeA.position.x, rangeB.position.x), 10f), Quaternion.Euler(0, 0, 90f));
+                //rb = meteo.GetComponent<Rigidbody2D>();
+                //Xvec = Random.Range(-10f, 10f);
+                //Yvec = Random.Range(-1f, -10f);
+                //radY = Mathf.Acos(Xvec / Mathf.Sqrt(Mathf.Pow(Xvec, 2) + Mathf.Pow(Yvec, 2))) * Mathf.Rad2Deg;
+                //// if (meteo.transform.position.x >= 0.01f) meteo.transform.localEulerAngles = new Vector3(0, 0, -90f);
+                //// else meteo.transform.localEulerAngles = new Vector3(0, 0, 180f);
+                //meteo.transform.localEulerAngles = new Vector3(0, 0, meteo.transform.localEulerAngles.z - radY);
+                //rb.velocity = new Vector3(Xvec, Yvec, 0);
                 break;
         }
     }
